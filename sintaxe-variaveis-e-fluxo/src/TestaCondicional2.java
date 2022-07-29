de086fa24bfac073f38a3ff807f74edf5f16e90e
Lidianne Barbosa <lidiannerb@gmail.com>
2022-07-28 23:59:02 -0300

    first commit

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 18;
+		boolean acompanhado = true;
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		}
+		else {
+			System.out.println("Você não pode entrar");
+		}
+	}
+}
